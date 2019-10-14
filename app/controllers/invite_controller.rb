5403149e03ef5b48612bc9fbb3031487fba0ff06
Tylor Schafer <tylorschafer@gmail.com>
2019-10-14 14:16:51 -0600

    Refactor redirect_issue after account verification
    Co-authored-by: Kelly Sanderson <46202366+ksandy95@users.noreply.github.com>

--- app/controllers/invite_controller.rb
+++ app/controllers/invite_controller.rb
@@ -6,13 +6,13 @@ class InviteController < ApplicationController
   def create
     service = GithubService.new(ENV['GITHUB_USER_TOKEN'])
     result = service.get_email(params['Github Handle'])
-    email = result[0][:payload][:commits][0][:author][:email]
+    email = result[0][:payload][:commits][0][:author][:email] unless result == []
     if email
       InviteMailer.invite_send(email, params['Github Handle'], current_user.first_name).deliver_now
       flash[:success] = "Successfully sent invite!"
-      redirect_to '/dashboard'
     else
       flash[:error] = "The Github user you selected doesn't have an email address associated with their account."
     end
+    redirect_to '/dashboard'
   end
 end
