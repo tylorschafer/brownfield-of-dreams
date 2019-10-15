849159af6134a9345f7728fb4b139368e1b73efc
Tylor Schafer <tylorschafer@gmail.com>
2019-10-14 20:02:55 -0600

    Add invite mailer testing

--- spec/mailers/user_status_spec.rb
+++ spec/mailers/user_status_spec.rb
@@ -1,5 +1,23 @@
 require "rails_helper"
 
 RSpec.describe UserStatusMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'activation' do
+    before :each do
+      @user = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+      @sent_mail = UserStatusMailer.activate(@user).deliver_now
+    end
+
+    it 'renders the correct subject' do
+      expect(@sent_mail.subject).to eq('Activate your account')
+    end
+
+    it 'renders the correct reciver email' do
+      expect(@sent_mail.to).to eq([@user.email])
+    end
+
+    it 'renders the correct sender email' do
+      expect(@sent_mail.from).to eq(['noreply@tylorandkellysapp.com'])
+    end
+  end
 end
