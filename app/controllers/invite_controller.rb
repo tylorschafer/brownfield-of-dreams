5f493e13a81dac414aad7563547ebca0e571d155
Tylor Schafer <tylorschafer@gmail.com>
2019-10-14 18:50:43 -0600

    Refactor invite_controller

--- app/controllers/invite_controller.rb
+++ app/controllers/invite_controller.rb
@@ -4,10 +4,8 @@ class InviteController < ApplicationController
   end
 
   def create
-    service = GithubService.new(ENV['GITHUB_USER_TOKEN'])
-    result = service.get_email(params['Github Handle'])
-    user = GithubUser.new(result)
-    sender_name = current_user.first_name
+    user = get_user
+    sender_name = "#{current_user.first_name} #{current_user.last_name}"
     if user.email
       InviteMailer.invite_send(user, sender_name).deliver_now
       flash[:success] = "Successfully sent invite!"
@@ -15,5 +13,11 @@ class InviteController < ApplicationController
       flash[:error] = "The Github user you selected doesn't have an email address associated with their account."
     end
     redirect_to '/dashboard'
+  end
+
+  def get_user
+    service = GithubService.new(ENV['GITHUB_USER_TOKEN'])
+    result = service.get_email(params['Github Handle'])
+    GithubUser.new(result)
   end
 end
