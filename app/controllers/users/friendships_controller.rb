2fb8326d7e5f51deeef79b974d5b9267848eaa43
Tylor Schafer <tylorschafer@gmail.com>
2019-10-15 20:22:29 -0600

    Refactor friendships create

--- app/controllers/users/friendships_controller.rb
+++ app/controllers/users/friendships_controller.rb
@@ -1,11 +1,19 @@
 class Users::FriendshipsController < ApplicationController
 
   def create
-    current_user.friendships << Friendship.create(
+    friendship = create_friendship
+    if friendship.save
+      current_user.reload
+      redirect_to dashboard_path
+    else
+      flash[:error] = friendship.errors.full_messages_to_sentence
+    end
+  end
+
+  def create_friendship
+    Friendship.create(
       user_id: current_user.id,
       friend_id: params[:friend_id].to_i
     )
-    current_user.reload
-    redirect_to dashboard_path
   end
 end
